2058a95fe837ebacf2f2337d5eca5f99c795067e
Renato Di Giacomo <renato_di_giacomo@hotmail.com>
2024-06-10 21:45:05 -0300

    update patch/put

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 const formData = document.getElementById("enviar")
+const nameData = document.getElementById("nomeData")
+const ageData = document.getElementById("idadeData")
 
 const postData = async (e) => {
     e.preventDefault()
     /*GetInputData */
-    const nameData = document.getElementById("nome")
-    const ageData = document.getElementById("idade")
 
     console.log(nameData.value)
     console.log(ageData.value)
@@ -37,12 +37,15 @@ const listData = document.getElementById("getData")
 const getData = async () => {
     const loading = document.querySelector(".loading");
     const listData = document.getElementById("list")
+    const formGet = document.getElementById("formGet")
+
     listData.innerHTML = ""
     try {
         loading.classList.add("active")
         const response = await fetch("http://localhost:3000/data");
         if (response.status === 200) {
             const dataJson = await response.json()
+
             dataJson.forEach(data => {
                 listData.innerHTML +=
                     `
@@ -51,19 +54,23 @@ const getData = async () => {
                             <p>${data.nome}</p>
                             <p>${data.idade}</p>
                         </div>
-                       <button class="delete-btn">Deletar</button>
+                       <button class="delete-btn">Atualizar</button>
                      </li>
                       `
             });
+
         } else {
             alert("Connection Failed 1")
         }
+
         document.querySelectorAll('.delete-btn').forEach(button => {
             button.addEventListener('click', event => {
                 const itemId = event.currentTarget.closest('[data-id]').dataset.id;
-                deleteData(itemId);
+                console.log(itemId)
+                getOneData(itemId)
             });
         });
+
 
     } catch (error) {
         alert("Connection Failed 2")
@@ -76,14 +83,17 @@ const getData = async () => {
 formData.addEventListener("submit", postData)
 listData.addEventListener("click", getData)
 
-const deleteData = (dataId) => {
-    console.log(dataId)
-    try {
-        fetch(`http://localhost:3000/data/${dataId}`, {
-            method: "DELETE"
-        })
-        getData()
-    } catch (error) {
-        alert("Não Deletou")
+
+
+const getOneData = async (dataComming) => {
+    const res = await fetch(`http://localhost:3000/data/${dataComming}`)
+    const data = await res.json()
+    console.log(data)
+    nameData.value = data.nome
+    ageData.value = data.idade
+
+    const dataname = (e) => {
+        console.log(e)
     }
+    nameData.addEventListener("onchange",dataname)
 }
